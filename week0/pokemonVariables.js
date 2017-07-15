f5e34094b3adfd8eda465e7a61b391b828a8cd40
Abakir Hanna <abakirhanna73@gmail.com>
2017-07-14 22:15:53 -0400

    printing out variables

---
+++ week0/pokemonVariables.js
@@ -0,0 +1,42 @@
+//First Pokemon
+var charizardHP = 99999999999999;
+var charizardNum = 3;
+var charizardType = "Fire/Dragon";
+var charizardBestMove = "Fire Blitz";
+var charizardLastEvo = true;
+
+//bonus
+var charizardAttack = 99999999999; 
+var charizardDefense = 999999999;
+var charizardSpeed = 9999999999999;
+function inputCharizard(hp){
+    charizardHP = hp;
+}
+
+console.log ("Charizard HP:"+charizardHP);
+console.log ("Charizard Attack:"+charizardAttack);
+console.log ("Charizard Defense:"+charizardDefense);
+console.log ("Charizard Speed:"+charizardSpeed);
+console.log ("Charizard Speed:"+charizardSpeed);
+
+//Second Pokemon
+var blastoiseHP = 99999999999999;
+var blastoiseNum = 3;
+var blastoiseType = "Water";
+var blastoiseBestMove = "Hydro Gun";
+var blastoiseLastEvo = true;
+
+var blastoiseAttack = 99999999999; 
+var blastoiseDefense = 999999999;
+var blastoiseSpeed = 9999999999999;
+
+//third pokemon
+var venusaurHP = 99999999999999;
+var venusaurNum = 3;
+var venusaurType = "Grass";
+var venusaurBestMove = "Leaf Blade";
+var venusuarLastEvo = true;
+
+var venusuarAttack = 99999999999; 
+var venusuarDefense = 999999999;
+var venusuarSpeed = 9999999999999;
