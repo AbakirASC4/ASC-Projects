2907259c6a83b6c45e7c09bc104a644ecabcd721
Abakir Hanna <abakirhanna73@gmail.com>
2017-07-14 22:03:30 -0400

    Using loops and conditionals to print pokemon roster

---
+++ week0/PokemonArray.js
@@ -0,0 +1,21 @@
+var myPokemonRoster = ["Charizard", "Blastoise", "venasaur", "Alakasm", "Lapris"];
+var firstToFight = myPokemonRoster[0];
+var secondToFight = myPokemonRoster[1];
+myPokemonRoster.pop();
+myPokemonRoster.push("Pikachu");
+myPokemonRoster.push("Raichu");
+myPokemonRoster.length;
+var RosterSize = myPokemonRoster.length;
+if(myPokemonRoster.length >=4){
+for (var i = 0; i<5; i++){
+    console.log(myPokemonRoster[i]);
+}
+for (var i = 5; i>0; i--){
+    console.log(myPokemonRoster[i]);
+}
+for (var i = 0; i<myPokemonRoster.length; i++){
+    console.log(myPokemonRoster[i]);
+}
+}else{
+    console.log("You Can’t Play! Not Enough Pokemon");
+}
