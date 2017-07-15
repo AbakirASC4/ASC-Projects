41398504c1e6da65f3f882840a0a27dee52db280
Abakir Hanna <abakirhanna73@gmail.com>
2017-07-14 21:58:46 -0400

    Practice using classes and objects

---
+++ week0/object.js
@@ -0,0 +1,32 @@
+var charizard = {
+    "attack": "Blaze",
+    "HP" : 266,
+    "legendary" : false,
+    "types" : ["Fire", "Flying"]
+};
+function Superhero(realName,power,sidekick,race,gender,hideout,nemesis,universe){
+    this.realName = realName;
+    this.power =  power;
+    this.sidekick = sidekick;
+    this.race = race;
+    this.gender = gender;
+    this.hideout = hideout;
+    this.nemesis = nemesis;
+    this.universe = universe;
+    this.talk = function talk(){
+        console.log("Hello, my name is " + this.realName);
+    }
+}
+
+var superman = new Superhero("Clark Kent", "Heat vision",
+                            false,"Kryptonian", "Male",
+                            "Metroplis", "lex Luther", "DC");
+
+
+function Pizza(sauce,cheese,toppings, size){
+    this.sauce = sauce;
+    this.cheese = cheese;
+    this.toppings = toppings;
+    this.size = size;
+}
+var pizza = new Pizza('tomato sauce', 'mozzerella', ['pineapples', 'ham', 'bacon'], 'XXl');
