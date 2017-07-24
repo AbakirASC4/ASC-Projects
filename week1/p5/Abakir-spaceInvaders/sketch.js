23e543f4b99ac48410f42c18d54b9314c13665e8
Abakir Hanna <abakirhanna73@gmail.com>
2017-07-24 09:04:00 -0400

    Space Invaders

--- week1/p5/Abakir-spaceInvaders/sketch.js
+++ week1/p5/Abakir-spaceInvaders/sketch.js
@@ -1,25 +1,31 @@
+var img1;
+function preload() {
+  img1 = loadImage("http://www.pontek.pl/1588-thickbox_default/naklejka-space-invaders.jpg");
+}
+
 function Aliens(xpos,ypos,width,height){
 this.xpos = xpos;
 this.ypos = ypos;
 this.width = width;
 this.height = height;
 }
-var alien = [];
-for (var i =0; i<6; i++){
-    alien.push(new Aliens(30 + (i*90),50,50,50));
-}
 
+var alien = [];
 function Shot(xpos,ypos,width,height){
 this.xpos = xpos;
 this.ypos = ypos;
 this.width = width;
 this.height = height;
 }
+
+var gameScreen = 0;
 var shots = [];
 var a = 0;
 var x =0;
-var y = 10;
+var y = 25;
 var speed = 5;
+var level = 1;
+var score = 0;
 
 function setup(){
     createCanvas(600, 600);
@@ -27,23 +33,60 @@ function setup(){
 }
 
 function draw(){
-    background("grey");
-    fill('green');
-    rect(((width/2) - 50) + x, 550, 100, 30);
-    rect(((width/2) - 15) + x, 530, 30, 20);
-    move();
-    drawAlien();
-    addshot();
-    disappear();
+    if (gameScreen == 0) {
+        initScreen();
     }
+    if (gameScreen == 1){
+        background("grey");
+        drawShip();
+        move();
+        drawAlien();
+        addshot();
+        disappear();
+        drawScore();
+        if (alien.length == 0){
+            levels();
+        }
+    }
+}
+function initScreen(){
+    image(img1, 0, 0);
+    fill("black");
+    textSize(50);
+    textAlign(CENTER);
+    text("Click Here To Start!   ", 300, 325);
+
+}
     
+ function drawShip(){
+    fill('green');
+    var shipXpos = (((width/2) - 50) + x); 
+    rect(shipXpos, 550, 100, 30);
+    rect(((width/2) - 15) + x, 530, 30, 20);
+        if (shipXpos >= 600 ){
+            x = -x +100 ;
+        }else if(shipXpos  <= -75){
+            x = -x;
+        }
+ }
 function move(){
-if (keyIsDown(LEFT_ARROW)){
-x -= 5;
+    if (keyIsDown(LEFT_ARROW)){
+        x -= 5;
+    }
+        if (keyIsDown(RIGHT_ARROW)){
+            x += 5;
+        }
 }
-if (keyIsDown(RIGHT_ARROW)){
-x += 5;
-}
+
+function addshot(){
+    for(var a = 0; a < shots.length; a++){
+        fill("white");
+        rect(shots[a].xpos, shots[a].ypos, shots[a].width, shots[a].height);
+        shots[a].ypos = shots[a].ypos - y;
+        if (shots[a].ypos <= 0){
+            shots.splice(a,1);
+        }
+    }
 }
 
 function keyPressed(){
@@ -51,37 +94,86 @@ function keyPressed(){
              shots.push(new Shot((((width/2) - 15) + x), 530, 30, 20));
         }
     }
-function addshot(){
-    for(var i = 0; i < shots.length; i++){
-        fill("white");
-        rect(shots[i].xpos, shots[i].ypos, shots[i].width, shots[i].height);
-        shots[i].ypos = shots[i].ypos - y;
-    }
-}
+
 var xspeed = 2;
-var yspeed = 0.2;
+var yspeed = 15;
 
 function drawAlien(){
-    for (var i =0; i <= alien.length-1; i++){
+    for (var s =0; s < alien.length; s++){
         fill("green");
-        rect(alien[i].xpos,alien[i].ypos + 50,alien[i].width,alien[i].height);
-        alien[i].ypos = alien[i].ypos + yspeed
-        alien[i].xpos = alien[i].xpos + xspeed
-        if(alien[i].xpos < 0 || alien[i].xpos > 100 + (i * 90)){
+        rect(alien[s].xpos,alien[s].ypos,alien[s].width,alien[s].height);
+        alien[s].xpos = alien[s].xpos + xspeed;
+        
+        if(alien[s].xpos < 0 || alien[s].xpos > 600 - (alien[s].width) ){
             xspeed = -xspeed;
+            for (var d =0; d < alien.length; d++){
+                alien[d].ypos = alien[d].ypos + yspeed;
             }
-}
+        }
+       if(alien[s].ypos >= 510){
+           background("black");
+           fill("white");
+           textSize(50);
+           textAlign(CENTER);
+           text("YOU LOSE", 305,260);
+       }
+        
+    }
 }
 function disappear(){
-    for (var i = 0; i < alien.length;i++){
+    for (var i = 0; i < alien.length; i++){
         for(var j = 0; j < shots.length; j++){
-        if (shots[j].xpos >= alien[i].xpos && shots[j].xpos <= alien[i].xpos 
-        + alien[i].width && shots[j].ypos >= alien[i].ypos && shots[j].ypos 
-        <= alien[i].ypos + alien[i].height) {
-         alien.splice(i,1);
-         shots.splice(j,1);
-        }
-        
+            if (shots[j].xpos >= (alien[i].xpos - alien[i].width) && shots[j].xpos <= (alien[i].xpos + alien[i].width)
+            && shots[j].ypos >= (alien[i].ypos - alien[i].height) && shots[j].ypos <= (alien[i].ypos + alien[i].height)) {
+                shots.splice(j, 1);
+                alien.splice(i, 1);
+                
+                score = score + 10;
+            }
         }
     }
+    if (score == 1000){
+        background("black");
+        fill("white");
+        textSize(50);
+        textAlign(CENTER);
+        text("YOU HAVE WON", 305,260);
+    }
+}
+function mousePressed(){
+    if (gameScreen == 0) {
+        if (mouseX >= 0 && mouseX <= 600 && mouseY >= 0 && mouseY <=600) {
+            gameScreen ++;
+        }
+    }
+}
+function levels(){
+    if (gameScreen == 1){
+        for (var n =0; n<6; n++){
+            alien.push(new Aliens(30 + (n*90),50,50,50));
+            if (score >= 60 && alien.length >= 6) {
+                for (var z =0; z<6; z++){
+                    alien.push(new Aliens(30 + (z*90),125,50,50));
+                    if (score >= 180 && alien.length >= 12){
+                        yspeed ++;
+                        for (var x=0; x<6; x++){
+                            alien.push(new Aliens(30 + (x*90),200,50,50));
+                            if (score >= 360 && alien.length >= 18){
+                                yspeed ++;
+                                for (var c =0; c<6; c++){
+                                    alien.push(new Aliens(30 +(c*90),275,50,50));
+                                }
+                            }
+                        }
+                    }
+                }
+            }  
+        }
+    }
+}
+
+function drawScore(){
+    fill('black');
+    textSize(30);
+    text("Score: " + score, 20, 500, 100, 100);
 }
